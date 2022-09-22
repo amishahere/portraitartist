e710d0576704b4dd7e2fd77b0a6ea00a1d641c15
Amisha <21112000amisha@gmail.com>
2022-09-22 22:41:09 +0530

    initial

---
+++ src/components/MenuItems.js
@@ -0,0 +1,65 @@
+export const MenuItems = [
+  {
+    title: 'Adult and Child',
+    path: '/Adult and Child',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Boating',
+    path: '/Boating',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Pets and People',
+    path: '/Pets and People',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Wedding',
+    path: '/Weddings',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Posthumous',
+    path: '/Posthumous',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Fallen Heroes',
+    path: '/Fallen Heroes',
+    cName: 'dropdown-link',
+  },
+]
+
+export const Menu = [
+  {
+    title: 'Self-Portrait',
+    path: '/Self-Portrait',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Mixed and Other',
+    path: '/Mixed and Other',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Pastel',
+    path: '/Pastel',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Corporate and Groups',
+    path: '/Corporate and Groups',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Sibling Indoors',
+    path: '/Sibling Indoors',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Sports,Non-Pro',
+    path: '/Sports,Non-Pro',
+    cName: 'dropdown-link',
+  },
+]
